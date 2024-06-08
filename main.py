c639a77ebf340f38bfd1071278051916273f337b
msomea <msomearaphael@gmail.com>
2024-06-08 12:11:26 +0300

    Creating default variables

---
+++ main.py
@@ -0,0 +1,10 @@
+# Declaration of Global variables
+global score, live
+
+# Creating default variables
+score = 0 , live = 10
+
+# Starting the game with message
+print(' Answer by providing correct answer')
+
+
